43cd07191840cb9af44629cd91bdbca258785966
Stav Kamil <stavkamil@h-MacBook-Pro-sl-Stav.local>
2020-05-31 15:12:32 +0300

    Updates

---
+++ precache-manifest.ad9e398561d6a76d653634865da8afb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f352a2747a2345bb738089169f163dab",
+    "url": "/colorpickerapp/index.html"
+  },
+  {
+    "revision": "f9641520a0984b117e55",
+    "url": "/colorpickerapp/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "649754f8747511338edc",
+    "url": "/colorpickerapp/static/css/main.127850da.chunk.css"
+  },
+  {
+    "revision": "f9641520a0984b117e55",
+    "url": "/colorpickerapp/static/js/2.abe512ab.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/colorpickerapp/static/js/2.abe512ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649754f8747511338edc",
+    "url": "/colorpickerapp/static/js/main.0cb62e44.chunk.js"
+  },
+  {
+    "revision": "99e5ab1d552448311056",
+    "url": "/colorpickerapp/static/js/runtime-main.053c1297.js"
+  }
+]);
